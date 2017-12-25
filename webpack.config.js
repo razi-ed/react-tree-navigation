4f9c8f20bf855a5206921b49fabf5d034e57374e
Razi <razi.mathilakath@gmail.com>
2017-12-25 17:19:51 +0530

    Initial setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// We are using node's native package 'path'
+// https://nodejs.org/api/path.html
+const path = require('path');
+
+// Constant with our paths
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'), // source folder path -> ADDED IN THIS STEP
+  JS: path.resolve(__dirname, 'src/js'),
+};
+
+// Webpack configuration
+module.exports = {
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js',
+  },
+  // Dev server configuration -> ADDED IN THIS STEP
+  // Now it uses our "src" folder as a starting point
+  devServer: {
+    contentBase: paths.SRC,
+  },
+};
